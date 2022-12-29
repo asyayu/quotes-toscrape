36ecb2b3877f6972207581ce8314719e3fd7a84a
Asya Yurchenko <55962321+asyayu@users.noreply.github.com>
2022-12-29 22:49:40 +0100

    Fix issue where last page wasn't being scraped

--- crawler.py
+++ crawler.py
@@ -1,34 +1,34 @@
 import requests
 from bs4 import BeautifulSoup
 from csv import DictWriter
+import time
 
 page_num = 1
 BASE_URL = "http://quotes.toscrape.com"
 URL = f"http://quotes.toscrape.com/page/{page_num}"
-keep_going = True
+next_page_url = "/page/1"
 
-with open("quotes.csv", "a") as csvfile:
+with open("quotes2.csv", "a") as csvfile:
     headers = ("Text", "Author", "Bio")
     csv_writer = DictWriter(csvfile, fieldnames=headers)
     csv_writer.writeheader()
-    while keep_going:
-        response = requests.get(f"{BASE_URL}/page/{page_num}")
+    while next_page_url:
+        response = requests.get(f"{BASE_URL}{next_page_url}")
         data = response.text
         soup = BeautifulSoup(data, "html.parser")
-        if soup.find(class_="next"):
-            all_quotes = soup.select(".quote")
-            for quote in all_quotes:
-                quote_text = quote.find(class_="text").get_text()
-                author = quote.find(class_="author").get_text()
-                link_ending = quote.find_all("a")[0]["href"]
-                bio_link = BASE_URL + link_ending
-                csv_writer.writerow({
-                    "Text": quote_text,
-                    "Author": author,
-                    "Bio": bio_link,
-                })
-            print(f"Finished scraping {BASE_URL}/page/{page_num}")
-            page_num += 1
-        else:
-            print("Finished")
-            keep_going = False
+        all_quotes = soup.select(".quote")
+        for quote in all_quotes:
+            quote_text = quote.find(class_="text").get_text()
+            author = quote.find(class_="author").get_text()
+            link_ending = quote.find_all("a")[0]["href"]
+            bio_link = BASE_URL + link_ending
+            csv_writer.writerow({
+                "Text": quote_text,
+                "Author": author,
+                "Bio": bio_link,
+            })
+        print(f"Finished scraping {BASE_URL}{next_page_url}")
+        next_btn = soup.find(class_="next")
+        next_page_url = next_btn.find("a")["href"] if next_btn else None
+        time.sleep(2)
+    print("Finished")
